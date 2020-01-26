592d935ee21b07e2ec727767cb97c142704a4b73
SWIFT <SWIFT@SWIFT>
2020-01-26 20:50:51 +0000

    Journalio

---
+++ Journalio/src/com/journalio/android/Activity_Profile.java
@@ -0,0 +1,163 @@
+package com.journalio.android;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Activity_Profile extends Activity {
+
+	TextView tab_1;
+	Button bttn_base_0, bttn_base_1;
+	TextView nav_tv_1;
+	TextView nav_tv_2;
+	
+	
+	Context ctx;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main_news_page_layout);
+		
+		ctx = Activity_Profile.this;
+		
+		//Reference ui elements
+
+		nav_tv_1 = (TextView )findViewById(R.id.nav_tv_1);
+		
+		nav_tv_2 = (TextView )findViewById(R.id.nav_tv_2);
+		
+		bttn_base_0=(Button)findViewById(R.id.base_bttn_0);
+		
+		bttn_base_1=(Button)findViewById(R.id.base_bttn_1);
+		
+		bttn_base_0.setText(getResources().getString(R.string.get_your_account_for_more_features));
+		
+		nav_tv_2.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if(v.getId()==nav_tv_2.getId()) {
+				switch (event.getActionMasked()){
+
+  						case MotionEvent.ACTION_DOWN:
+  						case MotionEvent.ACTION_POINTER_DOWN: {
+  							//change color or background  ( new )
+  							break;
+  						}
+
+  						case MotionEvent.ACTION_UP:
+  						case MotionEvent.ACTION_POINTER_UP: {
+  							
+  							//change color to default
+  							
+  							//do the action
+  							Intent activity_Post_intent = new Intent(Activity_Profile.this, Activity_MainNewsPage.class );
+  							startActivity(activity_Post_intent);
+  							
+  							break;
+  						}
+  						case MotionEvent.ACTION_MOVE:{
+  							
+  							//finger moved : deactivate the whole action 
+  							
+  							break;
+  						}
+  					}
+				}
+				return true;
+			}
+		});
+		
+		
+       nav_tv_2.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if(v.getId()==nav_tv_2.getId()) {
+				switch (event.getActionMasked()){
+
+  						case MotionEvent.ACTION_DOWN:
+  						case MotionEvent.ACTION_POINTER_DOWN: {
+  							//change color or background  ( new )
+  							break;
+  						}
+
+  						case MotionEvent.ACTION_UP:
+  						case MotionEvent.ACTION_POINTER_UP: {
+  							
+  							//change color to default
+  							
+  							//do the action
+  							Intent activity_Post_intent = new Intent(Activity_Profile.this, Activity_Poster.class );
+  							startActivity(activity_Post_intent);
+  							
+  							break;
+  						}
+  						case MotionEvent.ACTION_MOVE:{
+  							
+  							//finger moved : deactivate the whole action 
+  							
+  							break;
+  						}
+  					}
+				}
+				return true;
+			}
+		});
+		
+		
+		for(int x=0; x<3; x++) {
+			
+			//because we are in the Activity_MainNewsPage,
+			//turn off all the views under the tv except 
+			//the first one.
+			if(x==2){//trending
+				nav_V(3, "nav_v_", x).setVisibility(View.VISIBLE);
+			}else{
+				nav_V(3, "nav_v_", x).setVisibility(View.GONE);
+			}
+			
+		}
+		
+		
+	}
+	
+	
+	public TextView nav_TV(int total_num_i, String prefix_str, int tv_i){
+
+		String[] textView_tv_ids = new String[total_num_i];
+
+		TextView[] textView_full_IDs = new TextView[total_num_i];
+
+		textView_tv_ids[tv_i] = prefix_str+Integer.toString(tv_i);
+
+		int textView_tv_ID = ctx.getResources().getIdentifier(textView_tv_ids[tv_i], "id", "com.journalio.android");
+		textView_full_IDs[tv_i] = (TextView)findViewById(textView_tv_ID);
+
+		return textView_full_IDs[tv_i];
+	}
+	
+	public View nav_V(int total_num_i, String prefix_str, int v_i){
+
+		String[] view_ids = new String[total_num_i];
+
+		View[] view_full_IDs = new View[total_num_i];
+
+		view_ids[v_i] = prefix_str+Integer.toString(v_i);
+
+		int view_ID = ctx.getResources().getIdentifier(view_ids[v_i], "id", "com.journalio.android");
+		view_full_IDs[v_i] = (View)findViewById(view_ID);
+
+		return view_full_IDs[v_i];
+	}
+	
+}
